3871ae00b14c0145f702be6a7861085db79466b7
Colin Campbell <prontobol@gmail.com>
2022-04-08 19:46:01 -0500

    correct csrf error

--- src/components/UploadForm.vue
+++ src/components/UploadForm.vue
@@ -1,29 +1,44 @@
 <template>
   <form @submit.prevent="uploadPhoto" id="uploadForm">
-    <input type="text" name="photo"/>
-    <input type="file" name="description"/>
+    <input type="text" name="photo" />
+    <input type="file" name="description" />
     <input type="submit" />
   </form>
 </template>
 <script>
 export default {
   created() {
-    console.log("Created");
+    this.getCsrfToken();
+  },
+  data: () => {
+    return {
+      csrf_token: "",
+    };
   },
   methods: {
+    getCsrfToken() {
+      let self = this;
+      fetch("/api/csrf-token")
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          self.csrf_token = data.csrf_token;
+        });
+    },
     uploadPhoto() {
-
-    
       let uploadForm = document.getElementById("uploadForm");
 
-      console.log(uploadForm)
+      console.log(uploadForm);
       let form_data = new FormData(uploadForm);
 
-      console.log(form_data)
+      console.log(form_data);
 
       fetch("/api/upload", {
         method: "POST",
-        body:form_data
+        body: form_data,
+        headers: {
+          "X-CSRFToken": this.csrf_token,
+        },
       })
         .then(function (response) {
           return response.json();
--- src/views/UploadView.vue
+++ src/views/UploadView.vue
@@ -7,23 +7,11 @@ import UploadForm from "./../components/UploadForm.vue";
 export default {
   components: { UploadForm },
   created() {
-      this.getCsrfToken()
+      
   },
-  data: () => {
-    return {
-      csrf_token: "",
-    };
-  },
+  
   methods: {
-    getCsrfToken() {
-      let self = this;
-      fetch("/api/csrf-token")
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-          self.csrf_token = data.csrf_token;
-        });
-    },
+    
   },
 };
 </script>
