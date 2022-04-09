50040e941c92294cb996867f6c137ac15994377a
Colin Campbell <prontobol@gmail.com>
2022-04-08 19:04:14 -0500

    Added upload form

---
+++ src/components/UploadForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <form @submit.prevent="uploadPhoto">
+      <input type="text"/>
+      <input type="file"/>
+      <input type="submit"/>
+  </form>
+</template>
+<script>
+export default {
+  created() {
+    console.log("Created");
+  },
+  methods: {
+    uploadPhoto() {
+      fetch("/api/upload", {
+        method: "POST",
+      })
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          // display a success message
+          console.log(data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,31 @@
 <script>
+import UploadForm from "./../components/UploadForm.vue";
 export default {
-    data() {
-        return {
-            message: "Hello World! This is Lab 7"
-        };
-    },
-}
+  data() {
+    return {
+      message: "Hello World! This is Lab 7",
+    };
+  },
+
+  components: {UploadForm},
+};
 </script>
 
 <template>
-    <div class="container">
-      <div class="text-center">
-        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-        <h1>{{ message }}</h1>
-      </div>
+  <div class="container">
+    <div class="text-center">
+      <UploadForm></UploadForm>
+
+      <img
+        alt="Vue logo"
+        class="logo"
+        src="@/assets/logo.svg"
+        width="125"
+        height="125"
+      />
+      <h1>{{ message }}</h1>
     </div>
+  </div>
 </template>
 
 <style>
