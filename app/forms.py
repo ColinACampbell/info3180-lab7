0e3345292e054509c137586ab1c70c3cef147fe5
Colin Campbell <prontobol@gmail.com>
2022-04-10 20:25:30 -0500

    images allowed

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileAllowed
 from wtforms import FileField
 from wtforms import TextAreaField
 from wtforms.validators import DataRequired
 class UploadForm (FlaskForm):
     description = TextAreaField(validators=[DataRequired()])
-    photo = FileField(validators=[DataRequired()])
+    photo = FileField(validators=[DataRequired(),FileAllowed(['jpg','jpeg','gif','png'],'Images Allowed')])
